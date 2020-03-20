f8d83de32e86d93610a00a2b6dc8f5b6cb58c02d
Dilpreet Singh <42351992+dilpreet-is-technophile@users.noreply.github.com>
2020-03-20 12:06:20 +0530

    Add files via upload

---
+++ FinalVariable.java
@@ -0,0 +1,21 @@
+class finalvar {
+    final int ABC;
+
+    public finalvar() {
+        ABC = 10;
+        ABC = 20; // this cannot be done it will give error
+
+    }
+
+    public void printit() {
+        System.out.println(ABC);
+    }
+
+}
+
+class FinalVariable {
+    public static void main(String args[]) {
+        finalvar obj = new finalvar();
+        obj.printit();
+    }
+}
